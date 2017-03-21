8845c92b26dbc2e41d09cb3f574e1ed543ccec70
fatandygoode <johnmadden86@gmail.com>
2017-03-21 20:51:16 +0000

    Add files via upload

--- BoardGame/src/models/Player.java
+++ BoardGame/src/models/Player.java
@@ -1,11 +1,11 @@
 package models;
 
-public class Player
-{
+public class Player {
 	private String playerName;
 	private int numberOfCarrots;
-	private int currentPosition;
 	private int numberOfLettuces;
+	private int boardPosition;
+	private int racePosition;
 	
 	/**
      * Constructor for objects of class Player
@@ -13,47 +13,91 @@ public class Player
      * @param playerName Name of the player
      * 
      */
-	
-	public Player(String playerName)
-    {
+	public Player(String playerName) {
         this.setPlayerName(playerName);
-  
 		setNumberOfCarrots(65);
-		
-		setCurrentPosition(64);
-		
 		setNumberOfLettuces(3);
+		setBoardPosition(0);
+		setRacePosition(0);	
     }
-
+	
+	/**
+	 * Method to get a player's name
+	 * @return the player's name
+	 */
 	public String getPlayerName() {
 		return playerName;
 	}
 
+	/**
+	 * Method to change a player's name
+	 * @param playerName - the new name for the player
+	 */
 	public void setPlayerName(String playerName) {
 		this.playerName = playerName;
 	}
 
+	/**
+	 * Method to get the number of carrots a player has
+	 * @return the number of carrots the player has
+	 */
 	public int getNumberOfCarrots() {
 		return numberOfCarrots;
 	}
 
+	/**
+	 * Method to update the number of carrots a player has
+	 * @param numberOfCarrots - the new number of carrots for the player
+	 */
 	public void setNumberOfCarrots(int numberOfCarrots) {
 		this.numberOfCarrots = numberOfCarrots;
 	}
 
-	public int getCurrentPosition() {
-		return currentPosition;
-	}
-
-	public void setCurrentPosition(int currentPosition) {
-		this.currentPosition = currentPosition;
-	}
-
+	/**
+	 * Method to get the number of lettuces a player has left
+	 * @return the number of lettuce the player has left
+	 */
 	public int getNumberOfLettuces() {
 		return numberOfLettuces;
 	}
 
+	/**
+	 * Method to update the number of lettuces a player has
+	 * @param numberOfLettuces - the new number of lettuces for the player
+	 */
 	public void setNumberOfLettuces(int numberOfLettuces) {
 		this.numberOfLettuces = numberOfLettuces;
+	}
+
+	/**
+	 * Method to get a player's current board position
+	 * @return the player's current board position
+	 */
+	public int getBoardPosition() {
+		return boardPosition;
+	}
+
+	/**
+	 * Method to move a player to a new board position
+	 * @param boardPosition - the player's new board position
+	 */
+	public void setBoardPosition(int boardPosition) {
+		this.boardPosition = boardPosition;
+	}
+
+	/**
+	 * Method to get a player's race position
+	 * @return the player's current place in the race
+	 */
+	public int getRacePosition() {
+		return racePosition;
+	}
+
+	/**
+	 * Method to update a player's race postion
+	 * @param racePosition - the player's new place in the race
+	 */
+	public void setRacePosition(int racePosition) {
+		this.racePosition = racePosition;
 	}
 }
