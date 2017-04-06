1ceecd9153f3cd326981a4674f69c862ccf47f24
Brianos-17 <brianos-17@hotmail.com>
2017-04-06 22:26:47 +0100

    Add files via upload
    Added JavaDoc comments.
    Commented out the addPlayer() method that we have to remember to take out of the final version
    Also not sure if we need the public ArrayList<Players> getPlayers() method

--- src/controllers/Board.java
+++ src/controllers/Board.java
@@ -2,211 +2,280 @@ package controllers;
 
 import models.Player;
 import models.Square;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
+@SuppressWarnings({"WeakerAccess"})
+public class Board {
+    private final ArrayList<Square> squares;
+    private final ArrayList<String> hareCards;
+    @SuppressWarnings("CanBeFinal")
+    private ArrayList<Player> players;
+    private Integer turnCounter, numberOfPlayersFinished, hareCardDeck;
 
-public class Board
-{
-	public static ArrayList<Square> squares;
-	public static ArrayList<Player> players;
-	public static ArrayList<String> hareCards;
-	public static Integer turnCounter, numberOfPlayersFinished, hareCardDeck;
-	
-	/**
-	 * Constructor for objects of class Board
-	 */
-	public Board() {
-		squares = new ArrayList<>();
-		createBoard();
-		players = new ArrayList<>();
-		addPlayers();//delete when finished
-		hareCards = new ArrayList<>();
-		createDeck();
-		turnCounter = 0;
-		numberOfPlayersFinished = 0;
-		hareCardDeck = 0;
-	}
+    /**
+     * Constructor for objects of class Board
+     * 
+     * @param squares Creates a new ArrayList of type Square
+     * @param players Creates a new ArrayList of type Player
+     * @param hareCards Creates a new ArrayList of type String
+     * @param turnCounter An int which gets increased at the end of each players turn
+     * @param numberOfPlayersFinished An int which keeps track of players who have finished the game
+     * @param hareCardDeck An int which keeps track of the position of hareCards in the ArrayList 
+     */
+    public Board() {
+        squares = new ArrayList<>();
+        createBoard();
+        players = new ArrayList<>();
+        addPlayers();//delete when finished
+        hareCards = new ArrayList<>();
+        createDeck();
+        turnCounter = 0;
+        numberOfPlayersFinished = 0;
+        hareCardDeck = 0;
+    }
 
-	private void createDeck() {
-		int i = 0;
-		while(i < 1){
-			hareCards.add(new String("Give 10 carrots to each player lying behind you in the race (if any).\nIf you haven't enough carrots, give them five each; if still not possible, one each.\nA player who doesn't want extra carrots may discard them"));
-			hareCards.add(new String("If there are more players behind you than in front of you, miss a turn. If not, play again. If equal, of course play again."));
-			hareCards.add(new String("Restore your carrot holding to exactly 65!"));
-			hareCards.add(new String("Draw 10 Carrots for each lettuce you still hold. If you have none left, miss a turn."));
-			hareCards.add(new String("Free Ride! Your Last turn costs nothing; retrieve the carrots you paid to reach this square"));
-			hareCards.add(new String("Lose half your carrots! If an odd number, keep the odd one."));
-			hareCards.add(new String("Show us your carrots! Show your carrots so everyone knows how many you have left"));
-			i++;
-		}
-		hareCards.add(new String("Shuffle The hare cards and receive from each player 1 carrot for doing so"));
-		Collections.shuffle(hareCards);
-	}
+    /**
+     *Populates the hareCards ArrayList with object of type String
+     *
+     */
+    @SuppressWarnings("RedundantStringConstructorCall")
+    private void createDeck() {
+        int i = 0;
+        while(i < 2){
+            hareCards.add(new String("Give 10 carrots to each player lying behind you in the race (if any).\nIf you haven't enough carrots, give them five each; if still not possible, one each.\nA player who doesn't want extra carrots may discard them"));
+            hareCards.add(new String("Restore your carrot holding to exactly 65!"));
+            hareCards.add(new String("Draw 10 Carrots for each lettuce you still hold. If you have none left, miss a turn."));
+            hareCards.add(new String("Free Ride! Your Last turn costs nothing; retrieve the carrots you paid to reach this square"));
+            hareCards.add(new String("Lose half your carrots! If an odd number, keep the odd one."));
+            hareCards.add(new String("Show us your carrots! Show your carrots so everyone knows how many you have left"));
+            hareCards.add(new String("If there are more players behind you than in front of you, miss a turn. If not, play again. If equal, of course play again."));
+            i++;
+        }
+        hareCards.add(new String("Shuffle the hare cards and receive from each player 1 carrot for doing so"));
+        Collections.shuffle(hareCards);
+    }
 
-	public ArrayList<String> getHareCards() {
-		return hareCards;
-	}
+    /**
+     *
+     * @return 
+     */
+    public ArrayList<String> getHareCards() {
+        return hareCards;
+    }
 
-	public String getHareCard(int index) {
-		return hareCards.get(index);
-	}
+    /**
+     *Method to return specific hareCard based on a given idex
+     * @param index
+     * @return hareCard from the ArrayList by index number
+     */
+    public String getHareCard(int index) {
+        return hareCards.get(index);
+    }
 
-	private void createBoard() {
-		int i = 0;
-		while(i < 65) {
-			squares.add(new Square());
-			i++;
-		}
+    /**
+     *Method to create the Board for the board game
+     *Iterates through a series of Squares and populates the squares ArrayList
+     */
+    private void createBoard() {
+    	int i = 0;
+    	while(i < 65) {
+    		squares.add(new Square());
+    		i++;
+    	}
+        
+    	squares.get(0).setSquareType("----Start---");
+    	squares.get(64).setSquareType("---Finish---");
+        
+        int[] carrot = {2,5,13,21,26,33,38,40,49,55,59,61,63};
+        for(int c : carrot){
+        	squares.get(c).setSquareType("---Carrot---");
+        }
+        int[] hare = {1,3,6,14,25,31,34,39,46,51,58,62};
+        for(int h : hare) {
+        	squares.get(h).setSquareType("----Hare----");
+        }
+        int[] lettuce = {10,22,42,57};
+        for(int l : lettuce){
+        	squares.get(l).setSquareType("--Lettuce---");
+        }	
+        int[] tortoise = {11,15,19,24,30,37,43,50,56};
+        for(int t : tortoise){
+        	squares.get(t).setSquareType("--Tortoise--");
+        }
+        int[] number156 = {7,16,32,48,60};
+        for(int n156 : number156){
+        	squares.get(n156).setSquareType("--#(1,5,6)--");
+        }
+        int[] number2 = {8,17,23,29,35,41,47,53};
+        for(int n2 : number2){
+        	squares.get(n2).setSquareType("----#(2)----");
+        }
+        int[] number3 = {4,12,20,28,36,44,52};
+        for(int n3 : number3){
+        	squares.get(n3).setSquareType("----#(3)----");
+        }
+        int[] number4 = {9,18,27,45,54};
+        for(int n4 : number4){
+        	squares.get(n4).setSquareType("----#(4)----");
+        }
+    }
 
-		squares.get(0).setSquareType("----Start---");
-		squares.get(64).setSquareType("---Finish---");
+    /**
+     *Method to return Square from the squares Arraylist based on a given index
+     * 
+     * @param index
+     * @return Square at given index
+     */
+    public Square getSquare(int index) {
+        return squares.get(index);
+    }
 
-		int[] carrot = {2,5,13,21,26,33,38,40,49,55,59,61,63};
-		for(int c : carrot){
-			squares.get(c).setSquareType("---Carrot---");
-		}
-		int[] hare = {1,3,6,14,25,31,34,39,46,51,58,62};
-		for(int h : hare) {
-			squares.get(h).setSquareType("----Hare----");
-		}
-		int[] lettuce = {10,22,42,57};
-		for(int l : lettuce){
-			squares.get(l).setSquareType("--Lettuce---");
-		}	
-		int[] tortoise = {11,15,19,24,30,37,43,50,56};
-		for(int t : tortoise){
-			squares.get(t).setSquareType("--Tortoise--");
-		}
-		int[] number156 = {7,16,32,48,60};
-		for(int n156 : number156){
-			squares.get(n156).setSquareType("--#(1,5,6)--");
-		}
-		int[] number2 = {8,17,23,29,35,41,47,53};
-		for(int n2 : number2){
-			squares.get(n2).setSquareType("----#(2)----");
-		}
-		int[] number3 = {4,12,20,28,36,44,52};
-		for(int n3 : number3){
-			squares.get(n3).setSquareType("----#(3)----");
-		}
-		int[] number4 = {9,18,27,45,54};
-		for(int n4 : number4){
-			squares.get(n4).setSquareType("----#(4)----");
-		}
-	}
+    /**
+     *Method to return a human readable version of the board using a toString() method
+     *
+     * @return A string version of the board 
+     */
+    public String listSquares() {
+        String list = "";
+        for (int i = 1; i < squares.size()-1; i = i + 3) {
+            //noinspection StringConcatenationInLoop
+            list = list + i + squares.get(i).toString() + "\t\t\t" + (i+1) + squares.get(i+1) + "\t\t\t" + (i+2) + squares.get(i+2) + "\n";
+        }
+        return 0 + squares.get(0).toString() + "\n" + list + 64 + squares.get(64).toString();
+    }
 
-	public ArrayList<Square> getSquares() {
-		return squares;
-	}
+    /**
+     *Method which has to be removed before final version
+     *
+     */
+    private void addPlayers(){ //to help run the console quicker. delete when finished
+        players.add(new Player("John"));
+        players.add(new Player("Brian"));
+        players.add(new Player("Patrick"));
+        players.add(new Player("Sean"));
+    }
 
-	public Square getSquare(int index) {
-		return squares.get(index);
-	}
+    /**
+     *Method to add instances of class Player to the ArrayList players
+     *
+     * @param player
+     */
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
 
-	public static String listSquares() {
-		String list = "";
-		for (int i = 1; i < squares.size()-1; i = i + 3) {
-			list = list + i + squares.get(i).toString() + "\t\t\t" + (i+1) + squares.get(i+1) + "\t\t\t" + (i+2) + squares.get(i+2) + "\n";
-		}
-		return 0 + squares.get(0).toString() + "\n" + list + 64 + squares.get(64).toString();
-	}
+    /**
+     *Method to remove instance of class Player from the ArrayList players
+     *
+     * @param index of player to be removed
+     */
+    public void removePlayer(int index) {
+        if ( (index >= 0) && (index < players.size() ) ) {
+            players.remove(index);
+        }
+    }
 
-	private void addPlayers(){ //to help run the console quicker. delete when finished
-		players.add(new Player("John"));
-		players.add(new Player("Brian"));
-		players.add(new Player("Patrick"));
-		players.add(new Player("Sean"));
-	}
+    /**
+     *Method which returns the number of players currently in the ArrayList
+     *
+     * @return number of players  as an int
+     */
+    public int numberOfPlayers() {
+        return players.size();
+    }
 
-	/**
-	 * Method to add an object of type Player to the arraylist players
-	 * @param player - the name of the player to be added
-	 */
-	public void addPlayer(Player player) {
-		players.add(player);
-	}
+    /**
+     *Method which returns a human readable version of all players in the ArrayList players
+     *
+     *@return A human readable string of the players currently in the ArrayList players
+     */
+    public String listPlayers() {
+        StringBuilder list = new StringBuilder();
+        for (int index = 0; index < players.size(); index++) {
+            list.append(index).append(" - ").append(players.get(index).getPlayerName()).append("\n");
+        }
+        if (list.toString().equals("")) {
+            return "No players";
+        }
+        else {
+            return list.toString();
+        }
+    }
 
-	/**
-	 * Method to remove a player from the game
-	 * @param index - the index of the player to be removed
-	 */
-	public void removePlayer(int index) {
-		if ( (index >= 0) && (index < players.size() ) ) {
-			players.remove(index);
-		}
-	}
+    /**
+     *Method which return a player in the ArrayList players based upon a given index
+     *
+     * @param index
+     * @return an instance of class Player in the ArrayList player 
+     */
+    public Player getPlayer(int index) {
+        return players.get(index);
+    }
 
-	/**
-	 * Method to get the number of players playing the game
-	 * @return the size of the array list players
-	 */
-	public static int numberOfPlayers() {
-		return players.size();
-	}
+    /**
+     *Do we need this method?
+     * @return
+     */
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
 
-	/**
-	 * Method to return an indexed list of the players added to the game
-	 * @return an indexed list of the players added to the game
-	 */
-	public static String listPlayers() {
-		String list = "";
-		for (int index = 0; index < players.size(); index++) {
-			list = list + index + " - " + players.get(index).getPlayerName() + "\n";
-		}
-		if (list.equals("")) {
-			return "No players";
-		}
-		else {
-			return list;
-		}
-	}
+    /**
+     *Method which increases the turnCounter variable at the end of the players turn
+     *resets to 0 after the total number of players has been reached
+     */
+    public void increaseCounter(){
+        turnCounter++; //increment turn counter by 1
+        turnCounter = turnCounter % numberOfPlayers(); //reset to zero after all players have played
+    }
 
-	/**
-	 * Method to get a player from the array list
-	 * @param index - the index of the player to be returned
-	 * @return the object of type Player corresponding to the given index
-	 */
-	public Player getPlayer(int index) {
-		return players.get(index);
-	}
+    /**
+     *Method which returns the turnCounter
+     * @return turnCounter as an int
+     */
+    public int getTurnCounter(){
+        return turnCounter;
+    }
 
-	public ArrayList<Player> getPlayers() {
-		return players;
-	}
-	
-	public int getTurnCounter()
-	{
-		return turnCounter;
-	}
-	
-	public static void increaseCounter()
-	{
-		turnCounter++; //increment turn counter by 1
-		turnCounter = turnCounter % numberOfPlayers(); //reset to zero after all players have played
-	}
-	
-	public int getNumberOfPlayersFinished()
-	{
-		return numberOfPlayersFinished;
-	}
-	
-	public static void newPlayerFinished()
-	{
-		numberOfPlayersFinished ++;
-	}
-	
-	public int getHareCardDeck()
-	{
-		return hareCardDeck;
-	}
-	
-	public static void drawNextCard()
-	{
-		hareCardDeck ++;
-	}
-	
+    /**
+     *Method which increases the value of the hareCard ArrayList, changing which card is drawn next when
+     *a hare square is landed on
+     */
+    public void drawNextCard() {
+        hareCardDeck++;
+    }
+
+    /**
+     *Method which shuffles the entire hareCard ArrayList after the "Shuffle" card has been drawn
+     */
+    public void shuffleDeck(){
+        hareCardDeck = 0;
+    }
+
+    /**
+     *Method which returns a first hare care in the ArrayList
+     * @return String from the ArrayList hareCards at index number 0
+     */
+    public int topOfDeck(){
+        return hareCardDeck;
+    }
+
+    /**
+     *Method which increases the numberOfPlayersFinished whenever a player reaches the "Finished" square
+     */
+    public void newPlayerFinished() {
+        numberOfPlayersFinished ++;
+    }
+
+    /**
+     *Method which returns the amount of players who have finished the game
+     *
+     * @return the numberOfPlayersFinished as an int
+     */
+    public int getNumberOfPlayersFinished(){
+        return numberOfPlayersFinished;
+    }
 }
 
 
