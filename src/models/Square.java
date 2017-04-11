9b3dac4eb2a9f4ca105ea65b01a88e23e5ba9076
mundy93 <20038833@mail.wit.ie>
2017-04-11 12:54:28 +0100

    Update Square.java

--- src/models/Square.java
+++ src/models/Square.java
@@ -1,4 +1,4 @@
-package models;
+package src.models;
 
 /**
  *
@@ -18,39 +18,42 @@ public class Square
 
     /**
      *
-     * @return
+     * This method lets user know whether its a Hare,Tortise,Carrot,Lettuce or Number Square
+     * @return the type of square
+     * 
      */
     public String getSquareType() {
         return squareType;
     }
 
     /**
-     *
-     * @param squareType
+     * Allows to set the type of square
+     * @param squareType the type of square
      */
     public void setSquareType(String squareType) {
         this.squareType = squareType;
     }
 
     /**
-     *
-     * @return
+     * Returns if there another player currently occupying a certain square
+     * @return if the square is occupied or not
      */
     public boolean isOccupied() {
         return isOccupied;
     }
 
     /**
-     *
-     * @param isOccupied
+     * Allows the user to set if the square is occupied or not
+     * @param isOccupied is the square occupied or not
      */
     public void setOccupied(boolean isOccupied) {
         this.isOccupied = isOccupied;
     }
 
     /**
-     *
-     * @return
+     * Allows the user to see what the square type is and
+     * whether it is occupied or not
+     * @return a string version of the square object .the square type and the current occupied status
      */
     public String toString() {
     	String occupied = "";
